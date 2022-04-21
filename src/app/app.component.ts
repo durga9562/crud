26ebfbe59025c8038a354c144676efd9c47980d6
Durga Prasad Donga <dp21422@corp.ojas-it.com>
2022-04-21 10:05:54 +0530

    prasad

---
+++ src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular  CRUD operation with an Array Developed By Shiva @9985538462';
+  employees = [
+    {name:"kumar", position:"Programmer"},
+    {name:"mani", position:"Designer"},
+    {name:"Amer", position:"Manager"}
+  ];
+  model:any={};
+  model2:any={};
+  msg:any="";
+  addEmployee(){
+    this.employees.push(this.model);
+    this.model = {};
+    this.msg = "Record is successfully added..... "; 
+
+  }
+  deleteEmployee(i:any){
+    this.employees.splice(i,1);
+    this.msg = "Record is successfully deleted..... ";
+    
+  }
+  myValue:any;
+  editEmployee(k:any){
+    this.model2.name = this.employees[k].name;
+    this.model2.position = this.employees[k].position;
+    this.myValue = k;
+
+  }
+  updateEmployee(){
+    let k= this.myValue;
+    for(let i=0; i<this.employees.length;i++){
+      if(i==k){
+        this.employees[i]= this.model2;
+        this.model2 = {};
+        this.msg = "Record is successfully updated..... ";
+      }
+    }
+  }
+  clickMe(){
+    this.msg = "";
+  }
+}
